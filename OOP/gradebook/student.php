d3fe8d2cd1411a3eeba9d52be83c355cceca958a
alamin-php <alamin.php100@gmail.com>
2023-02-23 16:57:05 +0600

    student class done

--- OOP/gradebook/student.php
+++ OOP/gradebook/student.php
@@ -1,8 +1,22 @@
 <?php
 class Student{
-    public function __construct()
+    protected $id;
+    protected $name;
+
+    public function __construct($id,$name)
     {
-        # code...
+       $this->id    = $id;
+       $this->name  = $name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
     }
 }
 ?>
