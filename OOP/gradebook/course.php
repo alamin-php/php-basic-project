1e03b1d6eeffb8df3bb706deca8aa535334876cc
alamin-php <alamin.php100@gmail.com>
2023-02-23 17:35:43 +0600

    added code

--- OOP/gradebook/course.php
+++ OOP/gradebook/course.php
@@ -1,8 +1,39 @@
 <?php
 class Course{
-    public function __construct()
+    protected $id;
+    protected $name;
+    protected $students = array();
+    protected $grades = array();
+
+    public function __construct($id,$name)
     {
-        # code...
+        $this->id   = $id;
+        $this->name = $name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function addStudent($student)
+    {
+        $this->students[] = $student;
+    }
+
+    public function getStudents()
+    {
+        return $this->students;
+    }
+
+    public function addGrade($studentId)
+    {
+        $this->grades
     }
 }
 ?>
