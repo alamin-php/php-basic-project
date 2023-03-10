744aad7d7f47fe783b498d0d0810854478021d02
Md. Al-Amin Sarker <dev.alaminsarker@gmail.com>
2023-03-10 23:41:17 +0600

    create function.php
    date time default function

---
+++ date_function/function.php
@@ -0,0 +1,26 @@
+<?php 
+    // $date = date_create('2023-03-10');
+    // date_add($date, date_interval_create_from_date_string('30 days'));
+    // echo date_format($date,'Y-m-d');
+
+    // echo PHP_EOL;
+
+    // $date1 = date_create('2023-03-10');
+    // date_sub($date1, date_interval_create_from_date_string('30 days'));
+    // echo date_format($date1,'Y-m-d');
+
+    // echo PHP_EOL;
+
+    // $date3 = date_create('2023-01-01');
+    // $date4 = date_create('2023-03-10');
+
+    // $diff = date_diff($date3,$date4);
+
+    // echo $diff->format("%R%a days");
+
+    // $date5 = date_create('2023-01-01');
+    // echo date_format($date5, 'd-m-Y H:i:s');
+
+    // $date6 = date_create();
+    // echo date_timestamp_get($date6);
+?>
