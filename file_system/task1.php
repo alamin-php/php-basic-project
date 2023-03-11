d4ae00d7a7b35643705424846345f8461373c49c
alamin-php <alamin.php100@gmail.com>
2023-03-11 14:37:52 +0600

    file read

--- file_system/task1.php
+++ file_system/task1.php
@@ -1,11 +1,19 @@
 <?php 
 // $filename = 'D:\xampp\htdocs\php-basic-project\file_system\data\test.txt';
 $filename = 'D:\xampp\htdocs\php-basic-project\file_system\data\attn.txt';
-$fp = fopen($filename, 'r');
-while($line = fgets($fp)){
-    echo $line;
+if(is_readable($filename)){
+    $fp = fopen($filename, 'r');
+    while($line = fgets($fp)){
+        // echo $line;
+    }
+    while($line = fgets($fp)){
+        // echo $line;
+    }
+    fclose($fp);
+    
+    $data = file($filename);
+    // echo $data[10];
+    print_r($data);
+}else{
+    echo 'This file has been intranal error';
 }
-while($line = fgets($fp)){
-    echo $line;
-}
-fclose($fp);
